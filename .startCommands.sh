4ec6f5c37e78cf81a3a67d13f0d98b5807141361
Avery Vine <avine@tripadvisor.com>
2018-08-20 13:25:06 -0400

    updated gitignore

--- .startCommands.sh
+++
@@ -1,7 +0,0 @@
-DISCORD="Mzg2OTk3MzYzNjE4NzQyMjcy.DZdf9A.-qwiCaiUp-etNqHjgrP03BdTKfg"
-RIOT="RGAPI-332390a6-bcf0-40d8-8830-4fc1aed5bcd9"
-GOOGLE="AIzaSyCwys46oVhdwlhQ7iNgtT56TtMUG5Nn3Qs"
-TWITCH="1pyk0xppawf9cy7gohkemfjcjq78aa"
-CHAMPIONGG="245e4f76b33c6b217115e7d14e7f00f2"
-
-discord=$DISCORD riot=$RIOT google=$GOOGLE twitchId=$TWITCH championGG=$CHAMPIONGG node app.js
