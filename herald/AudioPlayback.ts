50338fbbf5236d627e85a3837d76965f26c3aae4
Avery Vine <averyvine@icloud.com>
2021-05-09 17:51:18 -0400

    Fix edge cases surrounding disconnecting, more logging (#4)
    * added ThemeInfo class
    
    * theme queueing
    
    * fix bug around disconnecting
    
    * voice channel names, really fix disconnect bug

--- herald/AudioPlayback.ts
+++ herald/AudioPlayback.ts
@@ -30,17 +30,20 @@ class AudioPlayback {
     private async playNextTheme() {
         let nextTheme = this.themeQueue.shift();
         if (!nextTheme) {
+            console.log(`No theme queued.`);
             if (this.connection) {
-                this.connection.channel.leave()
+                console.log(`Disconnecting from ${this.connection.channel.name}.`);
+                this.connection.disconnect();
+                this.connection = undefined;
             }
-            this.connection = undefined;
             return;
         }
         let themeInfo = nextTheme.themeInfo;
         let voiceChannel = nextTheme.voiceChannel;
-        console.log(`Next queued theme: ${themeInfo.link} (voice channel ${voiceChannel})`);
+        console.log(`Preparing to play next queued theme: ${themeInfo.link} (voice channel ${voiceChannel.name})`);
         try {
             if (!this.connection || this.connection.channel != voiceChannel) {
+                console.log(`Not currently in a voice channel, or currently in a different one. Joining ${voiceChannel.name}.`);
                 this.connection = await voiceChannel.join();
             }
             let stream = ytdl(themeInfo.link);
@@ -48,21 +51,35 @@ class AudioPlayback {
             dispatcher.setVolume((themeInfo.volume || this.defaultVolume) / 100);
             dispatcher.on('start', () => {
                 setTimeout(() => {
+                    console.log(`Completed playback of current theme.`)
                     dispatcher.end();
-                    this.playNextTheme();
+                    if (this.connection && this.connection.channel !== voiceChannel) {
+                        console.log(`Switched mid-playback from ${voiceChannel.name} to ${this.connection.channel.name}. Don't try to play another theme, one should already be playing!`)
+                    } else {
+                        this.playNextTheme();
+                    }
                 }, (themeInfo.duration || this.defaultDuration) * 1000);
             });
         } catch (error) {
-            console.log(`Something went wrong when trying to play ${themeInfo.link} (voice channel ${voiceChannel}): ${error}`);
-            voiceChannel.leave();
-            if (this.connection) {
-                this.connection.channel.leave()
-            }
-            this.connection = undefined
+            console.log(`Something went wrong when trying to play ${themeInfo.link} (voice channel ${voiceChannel.name}): ${error}`);
+            this.playNextTheme();
         }
     }
 
     public async voiceStateUpdate(oldMember: GuildMember, newMember: GuildMember) {
+        let startImmediately = false;
+        if (!newMember.voiceChannel || (oldMember.voiceChannel && oldMember.voiceChannel !== newMember.voiceChannel)) {
+            console.log(`${newMember.displayName} has left ${oldMember.voiceChannel.name}.`);
+            if (oldMember.voiceChannel && oldMember.voiceChannel.members.size <= 1) {
+                console.log(`No other members in ${oldMember.voiceChannel.name}. Leaving too.`);
+                if (!newMember.voiceChannel) {
+                    oldMember.voiceChannel.leave();
+                    this.connection = undefined;
+                } else {
+                    startImmediately = true;
+                }
+            }
+        }
         if (newMember.voiceChannel !== oldMember.voiceChannel && newMember.voiceChannel) {
             console.log(`${newMember.displayName} has joined ${newMember.voiceChannel.name}.`);
             Database.getInstance().getTheme(newMember.id).then(async (themeInfo) => {
@@ -70,18 +87,12 @@ class AudioPlayback {
                 let queuedTheme = { themeInfo: themeInfo, voiceChannel: newMember.voiceChannel };
                 this.themeQueue.push(queuedTheme);
                 console.log(`Theme queued: ${themeInfo.link}`);
-                if (!this.connection) {
+                if (!this.connection || startImmediately) {
                     this.playNextTheme();
                 } else {
-                    console.log(`Not playing immediately, connection is currently established in channel ${this.connection.channel}`)
+                    console.log(`Not playing immediately, connection is currently established in channel ${this.connection.channel.name}`)
                 }
             });
-        }
-        if (!newMember.voiceChannel) {
-            console.log(`${newMember.displayName} has left ${oldMember.voiceChannel.name}.`);
-            if (oldMember.voiceChannel && oldMember.voiceChannel.members.size <= 1) {
-                this.playNextTheme();
-            }
         }
     }
 }
